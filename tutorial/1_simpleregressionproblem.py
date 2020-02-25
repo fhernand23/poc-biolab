f096f754783413730db13a2f9eba0a4191e82c95
fede <fede.herz@gmail.com>
2020-02-25 19:39:15 -0300

    some work and refactoring

--- tutorial/1_simpleregressionproblem.py
+++ tutorial/1_simpleregressionproblem.py
@@ -1,3 +1,6 @@
+# https://towardsdatascience.com/understanding-pytorch-with-an-example-a-step-by-step-tutorial-81fc5f8c4e8e
+import numpy as np
+
 # Data Generation
 np.random.seed(42)
 x = np.random.rand(100, 1)
@@ -16,3 +19,255 @@ val_idx = idx[80:]
 x_train, y_train = x[train_idx], y[train_idx]
 x_val, y_val = x[val_idx], y[val_idx]
 
+# Initializes parameters "a" and "b" randomly
+np.random.seed(42)
+a = np.random.randn(1)
+b = np.random.randn(1)
+
+print(a, b)
+
+# Sets learning rate
+lr = 1e-1
+# Defines number of epochs
+n_epochs = 1000
+
+for epoch in range(n_epochs):
+    # Computes our model's predicted output
+    yhat = a + b * x_train
+
+    # How wrong is our model? That's the error!
+    error = (y_train - yhat)
+    # It is a regression, so it computes mean squared error (MSE)
+    loss = (error ** 2).mean()
+
+    # Computes gradients for both "a" and "b" parameters
+    a_grad = -2 * error.mean()
+    b_grad = -2 * (x_train * error).mean()
+
+    # Updates parameters using gradients and the learning rate
+    a = a - lr * a_grad
+    b = b - lr * b_grad
+
+print(a, b)
+
+# Sanity Check: do we get the same results as our gradient descent?
+from sklearn.linear_model import LinearRegression
+linr = LinearRegression()
+linr.fit(x_train, y_train)
+print(linr.intercept_, linr.coef_[0])
+
+###############
+# Torch version
+###############
+import torch
+import torch.optim as optim
+import torch.nn as nn
+from torchviz import make_dot
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+# Our data was in Numpy arrays, but we need to transform them into PyTorch's Tensors
+# and then we send them to the chosen device
+x_train_tensor = torch.from_numpy(x_train).float().to(device)
+y_train_tensor = torch.from_numpy(y_train).float().to(device)
+
+# Here we can see the difference - notice that .type() is more useful
+# since it also tells us WHERE the tensor is (device)
+print(type(x_train), type(x_train_tensor), x_train_tensor.type())
+
+lr = 1e-1
+n_epochs = 1000
+
+print("Torch Autograd")
+torch.manual_seed(42)
+a = torch.randn(1, requires_grad=True, dtype=torch.float, device=device)
+b = torch.randn(1, requires_grad=True, dtype=torch.float, device=device)
+
+for epoch in range(n_epochs):
+    yhat = a + b * x_train_tensor
+    error = y_train_tensor - yhat
+    loss = (error ** 2).mean()
+
+    # No more manual computation of gradients!
+    # a_grad = -2 * error.mean()
+    # b_grad = -2 * (x_tensor * error).mean()
+
+    # We just tell PyTorch to work its way BACKWARDS from the specified loss!
+    loss.backward()
+    # Let's check the computed gradients...
+    # print(a.grad)
+    # print(b.grad)
+
+    # What about UPDATING the parameters? Not so fast...
+
+    # FIRST ATTEMPT
+    # AttributeError: 'NoneType' object has no attribute 'zero_'
+    # a = a - lr * a.grad
+    # b = b - lr * b.grad
+    # print(a)
+
+    # SECOND ATTEMPT
+    # RuntimeError: a leaf Variable that requires grad has been used in an in-place operation.
+    # a -= lr * a.grad
+    # b -= lr * b.grad
+
+    # THIRD ATTEMPT
+    # We need to use NO_GRAD to keep the update out of the gradient computation
+    # Why is that? It boils down to the DYNAMIC GRAPH that PyTorch uses...
+    with torch.no_grad():
+        a -= lr * a.grad
+        b -= lr * b.grad
+
+    # PyTorch is "clingy" to its computed gradients, we need to tell it to let it go...
+    a.grad.zero_()
+    b.grad.zero_()
+
+print(a, b)
+
+print("Optimizer (SGD)")
+torch.manual_seed(42)
+a = torch.randn(1, requires_grad=True, dtype=torch.float, device=device)
+b = torch.randn(1, requires_grad=True, dtype=torch.float, device=device)
+print(a, b)
+
+lr = 1e-1
+n_epochs = 1000
+
+# Defines a SGD optimizer to update the parameters
+optimizer = optim.SGD([a, b], lr=lr)
+
+for epoch in range(n_epochs):
+    yhat = a + b * x_train_tensor
+    error = y_train_tensor - yhat
+    loss = (error ** 2).mean()
+
+    loss.backward()
+
+    # No more manual update!
+    # with torch.no_grad():
+    #     a -= lr * a.grad
+    #     b -= lr * b.grad
+    optimizer.step()
+
+    # No more telling PyTorch to let gradients go!
+    # a.grad.zero_()
+    # b.grad.zero_()
+    optimizer.zero_grad()
+
+print(a, b)
+
+print("Loss (MSE) and optimizer (SGD)")
+torch.manual_seed(42)
+a = torch.randn(1, requires_grad=True, dtype=torch.float, device=device)
+b = torch.randn(1, requires_grad=True, dtype=torch.float, device=device)
+print(a, b)
+
+lr = 1e-1
+n_epochs = 1000
+
+# Defines a MSE loss function
+loss_fn = nn.MSELoss(reduction='mean')
+
+optimizer = optim.SGD([a, b], lr=lr)
+
+for epoch in range(n_epochs):
+    yhat = a + b * x_train_tensor
+
+    # No more manual loss!
+    # error = y_tensor - yhat
+    # loss = (error ** 2).mean()
+    loss = loss_fn(y_train_tensor, yhat)
+
+    loss.backward()
+    optimizer.step()
+    optimizer.zero_grad()
+
+print(a, b)
+
+# With a model
+print("Torch with Model")
+
+
+class ManualLinearRegression(nn.Module):
+    def __init__(self):
+        super().__init__()
+        # To make "a" and "b" real parameters of the model, we need to wrap them with nn.Parameter
+        self.a = nn.Parameter(torch.randn(1, requires_grad=True, dtype=torch.float))
+        self.b = nn.Parameter(torch.randn(1, requires_grad=True, dtype=torch.float))
+
+    def forward(self, x):
+        # Computes the outputs / predictions
+        return self.a + self.b * x
+
+
+torch.manual_seed(42)
+
+# Now we can create a model and send it at once to the device
+model = ManualLinearRegression().to(device)
+# We can also inspect its parameters using its state_dict
+print(model.state_dict())
+
+lr = 1e-1
+n_epochs = 1000
+
+loss_fn = nn.MSELoss(reduction='mean')
+optimizer = optim.SGD(model.parameters(), lr=lr)
+
+for epoch in range(n_epochs):
+    # What is this?!?
+    model.train()
+
+    # No more manual prediction!
+    # yhat = a + b * x_tensor
+    yhat = model(x_train_tensor)
+
+    loss = loss_fn(y_train_tensor, yhat)
+    loss.backward()
+    optimizer.step()
+    optimizer.zero_grad()
+
+print(model.state_dict())
+
+
+# With a nested model
+print("Torch with Linear Model")
+
+
+class LayerLinearRegression(nn.Module):
+    def __init__(self):
+        super().__init__()
+        # Instead of our custom parameters, we use a Linear layer with single input and single output
+        self.linear = nn.Linear(1, 1)
+
+    def forward(self, x):
+        # Now it only takes a call to the layer to make predictions
+        return self.linear(x)
+
+
+torch.manual_seed(42)
+
+# Now we can create a model and send it at once to the device
+model = LayerLinearRegression().to(device)
+# We can also inspect its parameters using its state_dict
+print(model.state_dict())
+
+lr = 1e-1
+n_epochs = 1000
+
+loss_fn = nn.MSELoss(reduction='mean')
+optimizer = optim.SGD(model.parameters(), lr=lr)
+
+for epoch in range(n_epochs):
+    # What is this?!?
+    model.train()
+
+    # No more manual prediction!
+    # yhat = a + b * x_tensor
+    yhat = model(x_train_tensor)
+
+    loss = loss_fn(y_train_tensor, yhat)
+    loss.backward()
+    optimizer.step()
+    optimizer.zero_grad()
+
+print(model.state_dict())
